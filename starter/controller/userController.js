df8d876a76a3369769a976152e6693b53beff63b
Jerrin Thomas <jerrinmachu@gmail.com>
2023-07-27 14:49:23 +0530

    Node ecommerce api completed

---
+++ starter/controller/userController.js
@@ -0,0 +1,84 @@
+const User = require("../models/User")
+const { StatusCodes } = require("http-status-codes")
+const CustomError = require("../errors")
+const { attachCookiesToResponse, createTokenUser, checkPermissions} = require("../utils")
+
+
+
+
+
+
+const getAllUsers = async (req, res) => {
+    console.log(req.user);
+    const users = await User.find({ role: 'user'}).select('-password')
+    res.status(StatusCodes.OK).json({ users})
+}
+
+const getSingleUser = async (req, res) => {
+    const user = await User.findOne({ _id:req.params.id}).select('-password')
+    if(!user){
+        throw new CustomError.NotFoundError(`No user with id : ${req.params.id}`)
+    }
+    checkPermissions(req.user, user._id)
+    res.status(StatusCodes.OK).json({ user})
+}
+
+const showCurrentUser = (req, res) => {
+    res.status(StatusCodes.OK).json( {user: req.user})
+}
+
+//Update user with findoneandUpdate
+
+// const updateUser = async (req, res) => {
+//    const { name, email } = req.body
+//    if(!name || !email) {
+//     throw new CustomError.BadRequestError(' Please provide both values')
+//    }
+//    const user = await User.findOneAndUpdate(
+//     { _id: req.user.userId}, { email, name }, { new: true, runValidators: true }
+
+//    )
+
+//    const tokenUser = createTokenUser(user)
+//    attachCookiesToResponse({ res, user: tokenUser})
+//    res.status(StatusCodes.OK).json({ user: tokenUser})
+// }
+
+// Update user with User.save
+const updateUser = async (req, res) => {
+   const { name, email } = req.body
+   if(!name || !email) {
+    throw new CustomError.BadRequestError(' Please provide both values')
+   }
+   const user = await User.findOne({ _id: req.user.userId })
+   user.email = email
+   user.name = name
+   await user.save()
+
+   const tokenUser = createTokenUser(user)
+   attachCookiesToResponse({ res, user: tokenUser})
+   res.status(StatusCodes.OK).json({ user: tokenUser})
+}
+const updateUserPassword = async (req, res) => {
+    const { oldPassword, newPassword }  = req.body 
+    if(!oldPassword || !newPassword){
+        throw new CustomError.BadRequestError('Please provide both values')
+    }
+    const user = await User.findOne({ _id : req.user.userId })
+    const isPasswordCorrect = await user.comparePassword(oldPassword)
+    if(!isPasswordCorrect){
+        throw new CustomError.UnauthenticatedError('Invalid Credentials')
+    }
+    user.password = newPassword
+    await user.save()
+    res.status(StatusCodes.OK).json({ msg: 'Success! Password updated.'})
+}
+
+
+module.exports = {
+    getAllUsers,
+    getSingleUser,
+    showCurrentUser,
+    updateUser,
+    updateUserPassword
+}
